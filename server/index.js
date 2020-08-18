93a0cce0aa2c59768d65841d1e19474087e56680
Alex Kolov <kolov.alex@gmail.com>
2020-08-19 00:00:30 +0200

    add history middleware

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 const path = require('path');
 const express = require('express');
+const history = require('connect-history-api-fallback');
 const model = require('./model');
 
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.json());
+app.use(history());
 app.use(express.static(path.join(__dirname, '../dist')));
+
 model.init();
 
 app.get('/api/routes', (req, res) => {
